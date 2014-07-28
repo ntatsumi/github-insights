7d9c56344eb483fd1d9d420eecca0ef855416875
Noriaki Tatsumi <noriaki.tatsumi@blackboard.com>
2014-07-28 01:51:37 -0400

    Deleted an unused import.

--- test/GithubApiTest.java
+++ test/GithubApiTest.java
@@ -1,7 +1,6 @@
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import model.GithubRepo;
 import org.junit.Test;
 import utils.GithubApi;
